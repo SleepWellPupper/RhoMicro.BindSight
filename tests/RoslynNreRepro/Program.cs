ddc1e37639b98cd8b1382623b9004d801f6f6ede
Paul Braetz <paul.braetz@rhomicro.com>
2025-06-14 22:16:52 +0200

    trimmed project

--- tests/RoslynNreRepro/Program.cs
+++ tests/RoslynNreRepro/Program.cs
@@ -1,34 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.CodeAnalysis.MSBuild;
 
-await new AstroDocumentationService().Run();
-
-internal class AstroDocumentationService
-{
-    public async Task Run()
-    {
-        var project = await GetProject();
-        var compilation = await project.GetCompilationAsync();
-
-        if (compilation.GetDiagnostics()
-                .Where(d => d is
-                    {
-                        Severity: DiagnosticSeverity.Error
-                    } or
-                    {
-                        IsWarningAsError: true
-                    }).ToList() is [_, ..] errors)
-        {
-            return;
-        }
-    }
-
-    private async Task<Project> GetProject()
-    {
-        var workspace = MSBuildWorkspace.Create();
-        var project = await workspace.OpenProjectAsync("../RoslynNreReproTarget/RoslynNreReproTarget.csproj");
-        return project;
-    }
-}
-
+var workspace = MSBuildWorkspace.Create();
+var project = await workspace.OpenProjectAsync("../RoslynNreReproTarget/RoslynNreReproTarget.csproj");
+var compilation = await project.GetCompilationAsync();
+compilation?.GetDiagnostics(); // NRE here
