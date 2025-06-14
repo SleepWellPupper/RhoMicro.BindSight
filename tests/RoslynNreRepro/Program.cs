03949578fc30fb8de183f82f747d0cfc25af99f4
Paul Braetz <paul.braetz@rhomicro.com>
2025-06-14 16:52:45 +0200

    Revert "remove repro proj"
    This reverts commit 11ffa362590fc6249a760a761a6c483e669ceade.

---
+++ tests/RoslynNreRepro/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.Logging;
+
+await new AstroDocumentationService().Run();
+
+internal class AstroDocumentationService
+{
+    public async Task Run()
+    {
+        var project = await GetProject();
+        var compilation = await project.GetCompilationAsync();
+
+        if (compilation.GetDiagnostics()
+                .Where(d => d is
+                    {
+                        Severity: DiagnosticSeverity.Error
+                    } or
+                    {
+                        IsWarningAsError: true
+                    }).ToList() is [_, ..] errors)
+        {
+            return;
+        }
+    }
+
+    private async Task<Project> GetProject()
+    {
+        var workspace = MSBuildWorkspace.Create();
+        var project = await workspace.OpenProjectAsync("../../../../../src/RhoMicro.BindSight/RhoMicro.BindSight.csproj");
+        return project;
+    }
+}
+
